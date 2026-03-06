08f54f1faef1df8ec57e768e4a8ebf6303989290
SP <sempad@mail.ru>
2026-03-06 13:38:19 +0500

    7th iteration

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -52,7 +52,7 @@ func RunningSpentCalories(steps int, weight, height float64, duration time.Durat
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
-	if steps < 0 || duration < 0 {
+	if steps <= 0 || duration <= 0 {
 		return 0
 	}
 	return Distance(steps, height) / duration.Hours()
